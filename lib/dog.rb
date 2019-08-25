835db6e58294b7501d8f7a6d3bc540392e5d45fe
Eric Loudon <loudon.eric@gmail.com>
2019-08-25 16:13:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,35 @@
-class Dog
+ class Dog
+  @@all = [ ]
+  @@names = [ ]
 
-  @@all = []
+   attr_accessor :name
 
-  attr_accessor :name
-
-  def initialize(name)
+   def initialize(name)
     @name = name
+    @@names << self.name
+    self.save
+  end
+
+   def save
     @@all << self
   end
 
-  def self.all
+   def self.all
     @@all
   end
 
-  def clear_all
-    @@all.clear
-  end
+   def self.print_all
+    names_list = [ ]
+    @@names.each do |thing|
+      if names_list.include?(thing) == false
+        names_list << "#{thing}"
+      end
+    end
+  puts names_list
 end
+
+   def self.clear_all
+    @@all.clear
+    @@names.clear
+  end
+end 
